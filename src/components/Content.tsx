b93190fe7ac8db835004e705622791adcb7b4485
OJailson17 <JaylsonO17@gmail.com>
2021-11-05 00:05:36 -0300

    challenge done

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
+import { MovieProps } from "../@types/movieProps";
+import { MovieCard } from "./MovieCard";
+
+import "../styles/content.scss";
+
+interface ContentProps {
+  movies: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+  return (
+    <main>
+      <div className="movies-list">
+        {props.movies.map((movie) => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
+import { GenreResponseProps } from "../@types/genreProps";
+import { Button } from "./Button";
+
+import "../styles/sidebar.scss";
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  setSelectedGenreId: React.Dispatch<React.SetStateAction<number>>;
+  selectedGenreId: number;
+}
+
+export function SideBar(props: SideBarProps) {
+  function handleClickButton(id: number) {
+    props.setSelectedGenreId(id);
+  }
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
